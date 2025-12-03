34ff1ed386e1586e73da150752c4b290a01caa4a
hienhhcc <thachdau16t@gmail.com>
2025-12-03 21:47:05 +0700

    .\55_jump-game.ts

---
+++ array/55_jump-game.ts
@@ -0,0 +1,13 @@
+function canJump(nums: number[]): boolean {
+  let index = nums.length - 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i + nums[i] >= index) {
+      index = i;
+    }
+  }
+  return index === 0;
+}
+
+console.log(canJump([2, 3, 1, 1, 4]));
+console.log(canJump([3, 2, 1, 0, 4]));
+console.log(canJump([2, 0]));
