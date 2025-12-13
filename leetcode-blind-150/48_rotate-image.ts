fadd4aaac2a8ff5dc182709019795d1858b75cbe
hienhhcc <thachdau16t@gmail.com>
2025-12-13 11:58:46 +0700

    .\48_rotate-image.ts

---
+++ leetcode-blind-150/48_rotate-image.ts
@@ -0,0 +1,26 @@
+function rotateImage(matrix: number[][]): void {
+  const n = matrix.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse();
+  }
+  console.log(matrix);
+}
+
+rotateImage([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
+rotateImage([
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+]);
