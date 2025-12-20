638788fa5f4e51cb8fd69c6ab27fdd5fa4447457
hienhhcc <thachdau16t@gmail.com>
2025-12-20 11:09:30 +0700

    .\141_linked-list-cycle.ts

---
+++ leetcode-blind-150/141_linked-list-cycle.ts
@@ -0,0 +1,19 @@
+import { arrayToList, ListNode } from "../linked-list/utils";
+
+function hasCycle(head: ListNode | null): boolean {
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (fast && fast.next) {
+    slow = slow?.next!;
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
+}
+
+// console.log(hasCycle(arrayToList([3, 2, 0, -4])));
+// console.log(hasCycle(arrayToList([1, 2])));
+// console.log(hasCycle(arrayToList([1])));
---
+++ leetcode-blind-150/224_basic-calculator.ts
@@ -0,0 +1,5 @@
+function calculate(s: string): number {}
+
+console.log(calculate("1 + 1"));
+console.log(calculate(" 2-1 + 2 "));
+console.log(calculate("(1+(4+5+2)-3)+(6+8)"));
