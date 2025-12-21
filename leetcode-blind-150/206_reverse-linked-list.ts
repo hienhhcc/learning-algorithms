4533a347f8541a079774eaf4e38999cb72378797
hienhhcc <thachdau16t@gmail.com>
2025-12-21 16:58:43 +0700

    .\206_reverse-linked-list.ts

---
+++ leetcode-blind-150/206_reverse-linked-list.ts
@@ -0,0 +1,19 @@
+import { arrayToList, ListNode } from "../linked-list/utils";
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev: ListNode | null = null;
+  let curr: ListNode | null = head;
+
+  while (curr) {
+    const temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+
+  return prev;
+}
+
+console.log(reverseList(arrayToList([1, 2, 3, 4, 5])));
+console.log(reverseList(arrayToList([1, 2])));
+console.log(reverseList(arrayToList([])));
---
+++ leetcode-blind-150/92_reverse-linked-list-ii.ts
@@ -0,0 +1,10 @@
+import { arrayToList, ListNode } from "../linked-list/utils";
+
+function reverseBetween(
+  head: ListNode | null,
+  left: number,
+  right: number
+): ListNode | null {}
+
+console.log(reverseBetween(arrayToList([1, 2, 3, 4, 5]), 2, 4));
+console.log(reverseBetween(arrayToList([5]), 1, 1));
