a0fad915ee4cf05ee7d6e442a8a8a75f7a470e91
hienhhcc <thachdau16t@gmail.com>
2025-12-22 11:00:36 +0700

    .\92_reverse-linked-list-ii.ts

--- leetcode-blind-150/92_reverse-linked-list-ii.ts
+++ leetcode-blind-150/92_reverse-linked-list-ii.ts
@@ -4,7 +4,49 @@ function reverseBetween(
   head: ListNode | null,
   left: number,
   right: number
-): ListNode | null {}
+): ListNode | null {
+  let curr: ListNode | null = head;
+
+  let index = 1;
+  let nodeBeforeReverse: ListNode | null = null;
+  let nodeAfterReverse: ListNode | null = null;
+  let curr1: ListNode | null = null;
+  let lastNodeToReverse: ListNode | null = null;
+  while (curr) {
+    if (index === left) {
+      curr1 = curr;
+    }
+
+    if (index === right) {
+      lastNodeToReverse = curr;
+    }
+
+    if (index === left - 1) {
+      nodeBeforeReverse = curr;
+    }
+
+    if (index === right + 1) {
+      nodeAfterReverse = curr;
+    }
+    curr = curr?.next ?? null;
+    index++;
+  }
+
+  let index1 = left;
+  let prev: ListNode | null = nodeAfterReverse;
+  while (curr1 && index1 <= right) {
+    const temp = curr1.next;
+    curr1.next = prev;
+    prev = curr1;
+    curr1 = temp;
+    index1++;
+  }
+  if (nodeBeforeReverse) {
+    nodeBeforeReverse.next = prev;
+  }
+
+  return left === 1 ? lastNodeToReverse : head;
+}
 
 console.log(reverseBetween(arrayToList([1, 2, 3, 4, 5]), 2, 4));
-console.log(reverseBetween(arrayToList([5]), 1, 1));
+// console.log(reverseBetween(arrayToList([5]), 1, 1));
