93a340fcaf22a451d1b431a24bb9d43a9fce9a76
hienhhcc <thachdau16t@gmail.com>
2025-12-22 11:23:19 +0700

    .\83_delete-duplicated-from-sorted-list.ts

--- leetcode-blind-150/83_delete-duplicated-from-sorted-list.ts
+++ leetcode-blind-150/83_delete-duplicated-from-sorted-list.ts
@@ -1,5 +1,33 @@
-import { arrayToLinkedList, ListNode } from "../linked-list/utils";
+import {
+  arrayToLinkedList,
+  ListNode,
+  printLinkedList,
+} from "../linked-list/utils";
 
-function deleteDuplicates(head: ListNode | null): ListNode | null {}
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (head == null) {
+    return null;
+  }
 
-console.log(deleteDuplicates(arrayToLinkedList));
+  let dummy = head;
+  let hashset = new Set<number>();
+  hashset.add(head.val);
+  let prev = head;
+  let curr = head.next;
+
+  while (curr) {
+    if (hashset.has(curr.val)) {
+      prev.next = curr.next;
+      curr = curr.next;
+    } else {
+      prev = curr;
+      hashset.add(curr.val);
+      curr = curr.next;
+    }
+  }
+
+  return dummy;
+}
+
+printLinkedList(deleteDuplicates(arrayToLinkedList([1, 1, 2])));
+printLinkedList(deleteDuplicates(arrayToLinkedList([1, 1, 2, 3, 3])));
