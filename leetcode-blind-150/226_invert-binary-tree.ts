e87ea4097a8237ef664d8d3f98b20a47b175ce4c
hienhhcc <thachdau16t@gmail.com>
2025-12-24 12:12:34 +0700

    .\226_invert-binary-tree.ts

---
+++ leetcode-blind-150/226_invert-binary-tree.ts
@@ -0,0 +1,19 @@
+import { arrayToTree, TreeNode } from "../binary-trees/utils";
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root == null) return null;
+
+  //invert
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
+
+console.log(invertTree(arrayToTree([4, 2, 7, 1, 3, 6, 9])));
+console.log(invertTree(arrayToTree([2, 1, 3])));
+console.log(invertTree(arrayToTree([])));
